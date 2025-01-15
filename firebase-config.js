321a675c3a1d06d2e6dfebf3d6e915075e3ef171
Filbert Fuvian <ffuvian@gmail.com>
2025-01-15 16:45:19 +0700

    a

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMpR-NremqsuRknefCiXd6tiWsgNjjXRQ",
+  authDomain: "skill-lab-977b7.firebaseapp.com",
+  projectId: "skill-lab-977b7",
+  storageBucket: "skill-lab-977b7.firebasestorage.app",
+  messagingSenderId: "364210468649",
+  appId: "1:364210468649:android:ba01294cdb405fe7846938",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
+export default app;
