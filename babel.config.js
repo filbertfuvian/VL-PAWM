7f4268368fa04708669334ec24f52f76f059542a
Filbert Fuvian <ffuvian@gmail.com>
2025-01-17 21:01:31 +0700

    update

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-    presets: [
-      "@babel/preset-env",
-      "@babel/preset-react",
-      "@babel/preset-typescript", // Added TypeScript preset
-      'module:metro-react-native-babel-preset',
-      "@react-native/babel-preset"   
-    ],
-    plugins: ["@babel/plugin-transform-class-static-block"]
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
   };
-  
+};
